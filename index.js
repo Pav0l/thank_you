dc36ae5b697e599937d7b832eb85536a6c19af49
Pav0l <pavol.karas@gmail.com>
2019-04-13 21:07:10 +0200

    Add HTTPS

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+// Force HTTPS in production
+app.use(function(req, res, next) {
+  if (
+    req.headers['x-forwarded-proto'] !== 'https' &&
+    process.env.NODE_ENV === 'production'
+  ) {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+});
+
 app.use(express.static('public'));
 app.use('/', express.static('index.html'));
 
